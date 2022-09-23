8173f264eb2e0cdd6d710ae8802ab769d582c2bf
Vaishu-30 <113538323+Vaishu-30@users.noreply.github.com>
2022-09-23 13:56:00 +0530

    Add files via upload

---
+++ scramble.py
@@ -0,0 +1,22 @@
+str1=input("Enter the string_1: ")
+str2=input("Enter the string_2: ")
+new_str=""
+l1=[]
+l2=[]
+if len(str1)!=len(str2):
+    print("False")
+else:
+    if len(str1)==1:
+        print(True)
+    else:
+        while str2 not in l2:
+                    for i in str1:
+                        l1.append(i)
+
+                    for i in range(0,len(l1)-1,2):
+                        [l1[i],l1[i+1]]=[l1[i+1],l1[i]]
+                        for j in l1:
+                            new_str+=l1[i]
+                            l2.append(new_str)
+                            print(l2)
+                            str1=new_str
